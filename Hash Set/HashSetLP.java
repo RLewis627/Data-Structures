14be339c62dbc1b5c041130ea9f32d3be61f1493
Rachel Lewis <60766726+RLewis627@users.noreply.github.com>
2021-02-25 23:51:54 -0800

    Add files via upload

---
+++ Hash Set/HashSetLP.java
@@ -0,0 +1,112 @@
+public class HashSetLP<T> implements HashSetInterface<T>
+{
+	private int arrayCounter;
+	private T[] internalArray = (T[]) new Object[157];
+		
+	public int size(){return arrayCounter;}
+		
+    public boolean isEmpty(){return arrayCounter == 0;}
+		
+    public boolean contains(T item){return get(item) != null;}
+		
+    public T[] toArray()
+	{
+		T[] tempArray = (T[]) new Object[157];
+		for(int i = 0; i < arrayCounter; i++)
+		{
+			if(internalArray[i] != null)
+				tempArray[i] = internalArray[i];
+		}
+		return tempArray;
+	}
+		
+    public boolean add(T item)
+	{
+		int objHash = hash(item);
+		int index = objHash;
+		do
+		{
+			if(internalArray[index] == null)
+			{
+				internalArray[index] = item;
+				arrayCounter++;
+				return true;
+			}
+			if(internalArray[index].equals(item))
+				return true;
+			index = (index +1) % 157;
+		}while(index != objHash);
+		return false;
+	}
+		
+    public T remove(T item)
+	{
+		if(!contains(item))
+			return null;
+		int index = hash(item);
+		while(item.equals(internalArray[index]))
+		{
+			T temp = internalArray[index];
+			internalArray[index] = null;
+			arrayCounter--;
+			return temp;
+		}
+		return null;
+	}
+		
+    public void clear()
+	{
+		internalArray = (T[]) new Object[157];
+		arrayCounter = 0;
+	}
+	/*This point onwards is helper methods */
+	public T get(T item)
+	{
+		int index = hash(item);
+		while(internalArray[index] != null)
+		{
+			if(internalArray[index].equals(item))
+				return item;
+		}
+		return null;
+	}
+	
+	public boolean equals(Object obj)
+	{
+		if(obj == this)
+			return true;
+		if(obj == null || obj.getClass() != this.getClass())
+			return false;
+		return false;
+	}
+		
+	public int hashCode(int I){return I;}
+	
+	public int hashCode(char C){return (int)C;}
+		
+	public int hashCode(long L){return (int)(L>>>32)+((int)L);}
+		
+	public int hashCode(double D)
+	{
+		long L = Double.doubleToLongBits(D);
+		return hashCode(L);
+	}
+		
+	public int hashCode(String S)
+	{
+		int hash = 0;
+		for(int i = 0; i < S.length(); i++)
+			hash = 31 * hash + S.charAt(i);
+		return hash;
+	}
+		
+	public int hashCode(T t) 
+	{
+    	final int prime = 31;
+    	int result = 1;
+		result = prime * result + (t != null ? t.hashCode() : 0);
+   	 	return result;
+	}
+		
+	public int hash(T item){return hashCode(item) % 157;}
+}
