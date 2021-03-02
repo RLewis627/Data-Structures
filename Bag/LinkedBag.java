d88bc1cb2344a2ee07ff1dd61068545683ee0129
Rachel Lewis <60766726+RLewis627@users.noreply.github.com>
2021-03-01 23:11:35 -0800

    Add files via upload

---
+++ Bag/LinkedBag.java
@@ -0,0 +1,171 @@
+public class LinkedBag<T> implements BagInterface<T> {
+    private int numberOfEntries;
+    private static final int MAX_CAPACITY = 10000;
+    private Node head;
+
+	public LinkedBag(){
+		head = null;
+		numberOfEntries = 0;
+	}
+	
+    /**
+     * Gets the current number of entries in this bag.
+     *
+     * @return The integer number of entries currently in the bag.
+     */
+    public int getCurrentSize() {
+        return numberOfEntries;
+    }
+
+    /**
+     * Sees whether this bag is empty.
+     *
+     * @return True if the bag is empty, or false if not.
+     */
+    public boolean isEmpty() {
+        return (head == null);
+    }
+
+    /**
+     * Adds a new entry to this bag.
+     *
+     * @param newEntry The object to be added as a new entry.
+     * @return True if the addition is successful, or false if not.
+     */
+    public boolean add(T newEntry) {
+        try{
+			if(numberOfEntries < MAX_CAPACITY){
+				head = new Node(newEntry, head);
+				++numberOfEntries;
+			}
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+			
+    }
+
+    /**
+     * Removes one unspecified entry from this bag, if possible.
+     *
+     * @return Either the removed entry, if the removal.
+     * was successful, or null.
+     */
+    public T remove() {
+        try{
+			Node temp = head;
+			if(temp != null){
+				head = head.next;
+				temp.next = null;
+				--numberOfEntries;
+			}
+			return temp.item;
+		}catch(Exception e){
+			return null;
+		}
+    }
+
+    /**
+     * Removes one occurrence of a given entry from this bag.
+     *
+     * @param anEntry The entry to be removed.
+     * @return True if the removal was successful, or false if not.
+     */
+    public boolean remove(T anEntry) {
+        try{
+			Node temp = head;
+			while(temp != null){
+				if(anEntry == temp.item){
+					head = head.next;
+					temp.next = null;
+					--numberOfEntries;
+					temp = temp.next;
+					}
+				}
+				return true;
+		}catch(Exception e){
+			return false;
+		}
+    }
+
+    /**
+     * Removes all entries from this bag.
+     */
+    public void clear() {
+		Node temp = head;
+		while(temp != null){
+			head = head.next;
+			temp.next = null;
+			--numberOfEntries;
+			temp = temp.next;
+			}
+    }
+
+    /**
+     * Counts the number of times a given entry appears in this bag.
+     *
+     * @param anEntry The entry to be counted.
+     * @return The number of times anEntry appears in the bag.
+     */
+    public int getFrequencyOf(T anEntry) {
+        int frequency = 0;
+		Node temp = head;
+		while(temp != null){
+			if(anEntry == temp.item)
+				frequency++;
+		temp = temp.next;
+		}
+		return frequency;
+    }
+
+    /**
+     * Tests whether this bag contains a given entry.
+     *
+     * @param anEntry The entry to locate.
+     * @return True if the bag contains anEntry, or false if not.
+     */
+    public boolean contains(T anEntry) {
+        Node temp = head;
+		while(temp != null){
+			if(anEntry == temp.item)
+				return true;
+		temp = temp.next;
+		}
+		return false;
+    }
+
+    /**
+     * Retrieves all entries that are in this bag.
+     *
+     * @return A newly allocated array of all the entries in the bag.
+     * Note: If the bag is empty, the returned array is empty.
+     */
+    public T[] toArray() {
+		T[] array = (T[]) new Object[numberOfEntries];
+		int num = getCurrentSize();
+		Node temp = head;
+		while(temp != null){
+			if(num >= 0){
+			array[num-1] = temp.item;
+			--num;
+			}
+		temp = temp.next;
+		}
+		return array;
+    }
+
+    // Required to implement linked storage
+    private class Node{
+        private T item;
+        private Node next;
+
+        private Node(T item) {
+            this(item, null);
+        }
+
+        private Node(T item, Node next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+}
